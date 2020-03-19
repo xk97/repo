868ac732c3fa2a45706cdbbefb05512ac969739a
xk97 <xhkong@mail.utexas.edu>
2020-03-19 09:57:16 -0500

    test

--- python_sql/test_pyodbc.py
+++ python_sql/test_pyodbc.py
@@ -3,11 +3,11 @@ import pandas as pd
 
 print(pyodbc.drivers())
 # ['SQL Server', 'SQL Server Native Client 11.0', 'SQL Server Native Client RDA 11.0', 'ODBC Driver 13 for SQL Server']
-driver = 'ODBC Driver 17 for SQL Server'
+driver = 'SQL Server'
 server = r'DESKTOP-YOGA\SQLEXPRESS'
 database = 'test'
 con_string = 'DRIVER={};SERVER={};DATABASE={};Trusted_Connection=yes'.format(driver, server, database)  #UID=user;PWD=password
 print(con_string)
-con = pyodbc.connect(con_string)
-df = pd.read_sql('SELECT TOP 10 * FROM {}'.format('usertable'), con)
+with pyodbc.connect(con_string) as con: 
+    df = pd.read_sql('SELECT TOP 10 * FROM {}'.format('usertable'), con)
 print(df.shape)
