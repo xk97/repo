ad76329bf903478b67b8c8e96711682d7a103dba
xk97 <xhkong@mail.utexas.edu>
2020-05-05 00:40:45 -0500

    arg

---
+++ get_arg_parser.py
@@ -0,0 +1,53 @@
+"""
+https://github.com/chris-chris/mlops-example
+"""
+import argparse
+
+
+def get_params():
+    """
+    Parse args params
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+
+    # data hyperparams
+    parser.add_argument("--input_size", type=int, default=10)
+
+    parser.add_argument("--framework", type=str, default="sklearn")
+    parser.add_argument("--keras_model", type=str, default="dense")
+    parser.add_argument("--sklearn_model", type=str, default="linear")
+    parser.add_argument("--loss", type=str, default="squared_loss")
+
+    # training hyperparams
+
+    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--epoch", default=10, type=int)
+    parser.add_argument("--lr", default=0.001, type=float)
+
+    args = parser.parse_args()
+
+    return args
+
+
+# @ex.automain
+# def run(args):
+#     """
+#     Run sacred experiment
+#     :param args:
+#     :return:
+#     """
+#     if args.framework == 'sklearn':
+#         test_loss = sk.train_sklearn(args)
+#     elif args.framework == 'keras':
+#         test_loss = ke.train_keras(args, ex)
+#     else:
+#         return None
+
+#     ex.log_scalar('loss', test_loss)
+
+#     return test_loss
+
+
+if __name__ == "__main__":
+    print(get_params())
