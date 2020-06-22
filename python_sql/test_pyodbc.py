74529849d8e583a18a6f1c9fc976770ff904c939
xk97 <xhkong@mail.utexas.edu>
2020-06-21 17:12:40 -0700

    m

--- python_sql/test_pyodbc.py
+++ python_sql/test_pyodbc.py
@@ -1,4 +1,6 @@
 import pyodbc
+import sqlalchemy
+import numpy as np
 import pandas as pd
 
 print(pyodbc.drivers())
@@ -10,4 +12,15 @@ con_string = 'DRIVER={};SERVER={};DATABASE={};Trusted_Connection=yes'.format(dri
 print(con_string)
 with pyodbc.connect(con_string) as con: 
     df = pd.read_sql('SELECT TOP 10 * FROM {}'.format('usertable'), con)
-print(df.shape)
+print(df.shape, df.info(), df)
+DB = {'servername': server,
+      'database': database,
+      'driver': 'driver=SQL Server'}
+    #   'driver': 'driver=SQL Server Native Client 11.0'}
+conn = sqlalchemy.create_engine('mssql+pyodbc://' + DB['servername'] + '/' + DB['database'] + "?" + DB['driver'])
+df2 = pd.DataFrame(np.array(range(20)).reshape((5, 4)), columns='user_id, useract, actdate, acttime'.split(', '))
+print(df2)
+df2.to_sql('usertable', con=conn, if_exists='append', index=False)
+with pyodbc.connect(con_string) as con: 
+    df3 = pd.read_sql('SELECT TOP 10 * FROM {}'.format('usertable'), con)
+print(df3)
