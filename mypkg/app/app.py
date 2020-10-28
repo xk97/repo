1c33895ca9c63def22eeec6c015705fd57418a95
xk97 <xhkong@mail.utexas.edu>
2020-10-27 20:48:10 -0500

    s

--- mypkg/app/app.py
+++ mypkg/app/app.py
@@ -7,7 +7,8 @@ if __name__ == "__main__":
     work_dir = Path(Path(__file__).parent)
     print(work_dir, __file__)
     work_dir.joinpath('tmp').mkdir(parents=True, exist_ok=True)
-    logging.basicConfig(filename=work_dir / 'tmp'/ 'app.log', level=logging.INFO, filemode='w')
+    logging.basicConfig(filename=work_dir / 'tmp' /
+                        'app.log', level=logging.INFO, filemode='w')
     logger = logging.getLogger()
     logger.info('pkg version {}'.format(mypkg.__version__))
     parser = argparse.ArgumentParser()
